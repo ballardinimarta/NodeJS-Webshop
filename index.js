442d9d75d2ba8c63dfd282b4957808e20e65cb77
ballardinimarta <marta.ballardini@gmail.com>
2021-03-10 09:10:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const userRoute = require("./routes/userRoute")
+require("dotenv").config();
+
+
+// SCSS Middleware Config
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'scss'),
+    dest: path.join(__dirname, 'public/style'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/style'
+}));
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+app.set("view engine", "ejs");
+app.use("/", userRoute);
+
+
+mongoose.connect(process.env.DATABASE_URL, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err) => {
+        if (err) return 
+        app.listen(process.env.PORT, ()=> {
+        console.log(`app is ready at http://localhost:${process.env.PORT}`) 
+    })
+})
