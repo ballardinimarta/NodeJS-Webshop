39d0e982a990dab1bad5034130faf93542dc17bc
ballardinimarta <marta.ballardini@gmail.com>
2021-03-10 16:57:18 +0100

    admin page functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const {adminHomeRender, adminAddProduct, adminEditProductRender, adminEditProduct,adminDeleteProduct} = require("../controller/admincontroller");
 const router = express.Router();
+
+router.get("/admin", adminHomeRender);
+router.post("/admin", adminAddProduct);
+
+router.get("/admin/edit/:id", adminEditProductRender)
+router.post("/admin/edit/:id", adminEditProduct)
+
+router.get("/admin/delete/:id", adminDeleteProduct)
+
 
 router.get("/", (req, res) => {
     res.send("Webshop");
