f82dec4319d9e8f139ffbf3f2cfd7e72d0a99c6e
ballardinimarta <marta.ballardini@gmail.com>
2021-03-11 13:53:20 +0100

    fix upload image on edit and add more styling

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,19 +1,8 @@
 const express = require("express");
-const {adminHomeRender, adminAddProduct, adminEditProductRender, adminEditProduct,adminDeleteProduct} = require("../controller/admincontroller");
 const {registerRender, registerSubmit} = require('../controller/registerController');
 const {loginRender, loginSubmit, logOut} = require('../controller/loginController');
-const verifyTokenAdmin = require("../middleware/verifyUserAdmin")
-const {upload} = require("./../middleware/upload");
 
 const router = express.Router();
-
-router.get("/admin",verifyTokenAdmin,  adminHomeRender);
-router.post("/admin",verifyTokenAdmin, upload.single("imageurl"),  adminAddProduct);
-
-router.get("/admin/edit/:id",verifyTokenAdmin, adminEditProductRender)
-router.post("/admin/edit/:id",verifyTokenAdmin, adminEditProduct)
-
-router.get("/admin/delete/:id", verifyTokenAdmin, adminDeleteProduct)
 
 router.get('/register', registerRender) 
 router.post('/register', registerSubmit) 
