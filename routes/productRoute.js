668845b6581ab869d8bc3c34fe434c57a8366915
cornelialundell <lundellcornelia@gmail.com>
2021-03-12 11:42:21 +0100

    pagination

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const {productPageRender} = require('../controller/productController')
+const {productPageRender, showMoreProducts, showLessProducts} = require('../controller/productController')
 const verifyUser = require('../middleware/verifyUser')
 
 router.get('/', productPageRender) 
 router.get('/productPage', verifyUser, productPageRender) 
+
+router.get('/showmore', showMoreProducts);
+router.get('/showless', showLessProducts);
 
 
 module.exports = router;
