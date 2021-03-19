7c9ad6df0356e0638cab34e4fda62b3c3761bf6b
ballardinimarta <marta.ballardini@gmail.com>
2021-03-19 09:35:50 +0100

    admintest

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,18 @@
 const multer = require("multer");
 
+var maxSize = 1 * 1000 * 1000;
+
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now()+ ".png")
-    }
-  })
-var upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+ ".png")
+  }
+})
+
+var upload = multer({ storage: storage, limits: { fileSize: maxSize }})
 
 module.exports = {
-    upload, storage
+  upload, storage
 }
